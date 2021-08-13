31383dc77d5526ff4f8342d4b3a2e331b8a50ecb
Harshal Rasal <harshalrasal792@gmail.com>
2021-08-13 18:52:31 -0400

    Posts and Form init

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import flashbacks from './images/flashbacks.png'
 
 const App = () => {
@@ -10,6 +12,20 @@ const App = () => {
                 <Typography variant="h2" alogn="center">Flashbacks</Typography>
                 <img src={flashbacks} alt="flashbacks" height="60"/>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
 
         </Container>
     );
