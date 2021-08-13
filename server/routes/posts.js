ee08e06da4142a0f65357448238718fe51c821bc
Harshal Rasal <harshalrasal792@gmail.com>
2021-08-13 18:03:40 -0400

    server post config

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,15 @@
 import express from 'express';
 
+import {getPosts, createPost} from '../controllers/posts.js'
+
 const router = express.Router();
 
 // localhost:5000/posts
 // http://localhost:5000/posts
 
-router.get('/', (req, res) => {
-    res.send('THIS WORKS!');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
+
 
 export default router;
 
