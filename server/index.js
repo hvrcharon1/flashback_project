855a189195d2ada50b6fcd6f14deb6e3c474d3bc
Harshal Rasal <harshalrasal792@gmail.com>
2021-08-13 17:08:17 -0400

    server connection successful

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://flashbackdb:CharoN1792@cluster0.wqpjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) 
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
