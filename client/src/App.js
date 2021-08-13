b8a54408f8764232128732b4487fda0bd4885bc2
Harshal Rasal <harshalrasal792@gmail.com>
2021-08-13 18:35:55 -0400

    localhost Image post test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import flashbacks from './images/flashbacks.png'
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1> 
-        </div>
+        <Container maxidth="lg"> 
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" alogn="center">Flashbacks</Typography>
+                <img src={flashbacks} alt="flashbacks" height="60"/>
+            </AppBar>
+
+        </Container>
     );
 }
 
