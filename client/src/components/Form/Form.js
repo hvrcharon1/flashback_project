fa19a9c25ad237abc9b99839a47787b3910fac03
Harshal Rasal <harshalrasal792@gmail.com>
2021-08-13 22:40:01 -0400

    image post test

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper} from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     const classes =  useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
@@ -25,7 +31,7 @@ const Form = () => {
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
                 <div className={classes.fileinput}><FileBase type='file' multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 }) } /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullwidth>Submit</Button>
-                <Button variant="contained" color="secondary" size="small" onClick={clear} fullwidth>Clear</Button>
+                <Button className={classes.buttonClear} variant="contained" color="secondary" size="small" onClick={clear} fullwidth>Clear</Button>
             </form>
         </Paper>
     );
